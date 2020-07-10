257e76532776e13b34ff9aa50cd72ef2dfa6c4ff
Itay Getraide <itaygetraide@gmail.com>
2020-07-11 01:07:51 +0300

    Basic block destruction

---
+++ Assets/Scripts/BlockDestructor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDestructor : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        BreakBlock();
+    }
+
+    void BreakBlock() {
+        if(!Input.GetMouseButton(0)) { return; }
+
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(!Physics.Raycast(ray, out hit)) { return; }
+
+        if(hit.transform.gameObject.tag != "Block") { return; }
+
+        hit.transform.gameObject.GetComponent<BreakableBlock>().Break();
+    }
+}
---
+++ Assets/Scripts/BreakableBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableBlock : MonoBehaviour
+{
+    public int ticksToBreak = 50;
+    private int ticksLeft;
+    private Renderer renderer;
+
+    public void Awake() {
+        ticksLeft = ticksToBreak;
+        renderer = GetComponent<Renderer>();
+    }
+
+    public void Break() {
+        ticksLeft--;
+
+        renderer.material.color = Color.Lerp(Color.red, Color.green, (float)ticksLeft/ticksToBreak);
+
+        if(ticksLeft <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
