49d28df9bcf4ff52218afc77da9df81bad409e95
Itay Getraide <itaygetraide@gmail.com>
2020-07-10 23:16:15 +0300

    Fix movement to change with rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,6 +23,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(transform.position + (movement * movementSpeed * Time.deltaTime));
+        rb.MovePosition(transform.position + (transform.TransformDirection(movement) * movementSpeed * Time.deltaTime));
     }
 }
