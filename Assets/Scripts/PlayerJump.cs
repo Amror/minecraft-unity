1bf572e62704a941c2df08cc24d6db583d0eb5a4
Amror <51158703+Amror@users.noreply.github.com>
2020-07-12 00:50:03 +0300

    THE PLAYER CAN JUMP HE CAN JUMP
    omg the player can fucking jump
    and more shit

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float JumpHeight;
+    private Rigidbody rb;
+    private bool GroundCollision = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && GroundCollision)
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddForce(Vector3.up * JumpHeight);
+            GroundCollision = false;
+            rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+        }
+    }
+
+  
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Layer")
+        {
+            GroundCollision = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Layer")
+        {
+
+            GroundCollision = false;
+        }
+    }
+}
